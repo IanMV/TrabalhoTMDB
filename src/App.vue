dfadebe4fcd7603d94bb191800e06a83c4f90e8e
IanMV <ianmatheusv54@gmail.com>
2025-10-23 09:21:20 -0300

    feat: added menu navigation in App.vue; added two other pages for tv genres and series genres

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
-<script setup>
-  import { ref, onMounted } from 'vue';
-  import api from './plugins/axios';
+<script setup></script>
 
-  const moviesGenres = ref([]);
-  const TVGenres = ref([]);
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
 
-  onMounted(async () => {
-    let response = await api.get('genre/movie/list?language=pt-BR');
-    moviesGenres.value = response.data.genres;
-    response = await api.get('genre/tv/list?language=pt-BR');
-    TVGenres.value = response.data.genres;
-  });
-</script>
+<style scoped>
+  nav {
+    display: flex;
+    justify-content: flex-start;
+    margin-bottom: 1rem;
+    column-gap: 2rem;
+  }
+</style>
